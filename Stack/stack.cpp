945756494a9665c8456cc19b8422ceb186dfc394
Dmitry Lazarev <diman.lazarev@gmail.com>
2012-12-03 20:09:10 +0400

    Initial commit from Xcode

---
+++ Stack/stack.cpp
@@ -0,0 +1,9 @@
+//
+//  stack.cpp
+//  Stack
+//
+//  Created by Dmitry Lazarev on 03.12.12.
+//  Copyright (c) 2012 Ilya Lazarev. All rights reserved.
+//
+
+#include "stack.h"
---
+++ Stack/stack.h
@@ -0,0 +1,29 @@
+//
+//  stack.h
+//  Stack
+//
+//  Created by Dmitry Lazarev on 03.12.12.
+//  Copyright (c) 2012 Ilya Lazarev. All rights reserved.
+//
+
+#ifndef __Stack__stack__
+#define __Stack__stack__
+
+#include <iostream>
+
+class Stack
+{
+private:
+    
+    struct element {
+        int data;
+        struct element *next;
+    };
+    
+    element *top;
+    
+public:
+    int push();
+    void pop(int a);
+};
+#endif /* defined(__Stack__stack__) */
